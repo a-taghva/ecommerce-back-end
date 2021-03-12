8ddf32e316bc222c2540ca95a9d488ba883bfd63
a-taghva <taghvali99@gmail.com>
2021-03-11 22:18:43 -0500

    get category by id and its associated products

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO Tag(tag_name) 
+VALUES 
+("tagName1"),
+("tagName2");
+
+INSERT INTO Category(category_name) 
+VALUES 
+("catName1"),
+("catName2");
+
+INSERT INTO Product(product_name, price, category_id) 
+VALUES 
+("Shampoo", "12.49", "2"),
+("Noodle", "4.49", "1");
