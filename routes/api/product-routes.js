80369c5c0d05af2752d661aedea47133b2632458
a-taghva <taghvali99@gmail.com>
2021-03-13 08:47:25 -0500

    find a single product by it's id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -29,7 +29,27 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: { exclude: 'category_id' },
+        include: [
+            {
+                model: Tag,
+                attributes: ['tag_name']
+            },
+            {
+                model: Category,
+                attributes: [ 'category_name' ]
+            }
+        ]
+    })
+        .then(dbProductData => res.json(dbProductData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // create new product
