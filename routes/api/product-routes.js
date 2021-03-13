b794c63eaf4b2aa6b16d877780348bf85b7b5122
a-taghva <taghvali99@gmail.com>
2021-03-13 07:18:06 -0500

    get all ProductTag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+    ProductTag.findAll({
+        include: [
+            {
+                model: Product,
+                attributes: ['product_name']
+            },
+            {
+                model: Tag,
+                attributes: ['tag_name']
+            }
+        ]
+    })
+        .then(dbProductData => res.json(dbProductData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // get one product
